bd2777b0eb32cc7246764828afe2bfe6ac00c64d
peace710 <nothingsuper@163.com>
2020-03-29 21:47:46 +0800

    AspectJ constructor call

--- ALifeAspectJ/src/main/java/me/peace/aspectJ/App.java
+++ ALifeAspectJ/src/main/java/me/peace/aspectJ/App.java
@@ -1,11 +1,13 @@
 package me.peace.aspectJ;
 
-import android.util.Log;
-
 public class App {
     private static final String TAG = App.class.getSimpleName();
 
-    public void callLogin(String name,String pwd){
+    public App() {
+        LogUtils.i(TAG, "App() called");
+    }
+
+    public void callLogin(String name, String pwd){
         LogUtils.i(TAG, "callLogin() called with: name = [" + name + "], pwd = [" + pwd + "]");
     }
 
---
+++ ALifeAspectJ/src/main/java/me/peace/aspectJ/AspectJConstructorCall.java
@@ -0,0 +1,63 @@
+package me.peace.aspectJ;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+public class AspectJConstructorCall {
+    private static final String TAG = AspectJConstructorCall.class.getSimpleName();
+
+    public static void main(String[] args) {
+        App app = new App();
+    }
+}
+
+@Aspect
+class ConstructorCallAspect{
+    private static final String TAG = ConstructorCallAspect.class.getSimpleName();
+
+    /**
+     *     public static void main(String[] args) {
+     *         JoinPoint var2 = Factory.makeJP(ajc$tjp_0, (Object)null, (Object)null);
+     *
+     *         try {
+     *             ConstructorCallAspect.aspectOf().beforeCallConstructor(var2);
+     *             new App();
+     *         } catch (Throwable var4) {
+     *             ConstructorCallAspect.aspectOf().afterCallConstructor(var2);
+     *             throw var4;
+     *         }
+     *
+     *         ConstructorCallAspect.aspectOf().afterCallConstructor(var2);
+     *     }
+     */
+
+    //构造方法没有返回值
+    // me.peace.aspectJ.App.new代表App的构造方法（.new代表为构造方法）
+    @Pointcut("call(public me.peace.aspectJ.App.new(..))")
+    public void callConstructor(){
+
+    }
+
+
+    @Before("callConstructor()")
+    public void beforeCallConstructor(JoinPoint joinPoint){
+        LogUtils.i(TAG, "beforeCallConstructor() called");
+    }
+
+    @After("callConstructor()")
+    public void afterCallConstructor(JoinPoint joinPoint){
+        LogUtils.i(TAG, "afterCallConstructor() called");
+    }
+
+
+    //针对构造方法@Around不生效
+    @Around("callConstructor()")
+    public void aroundCallConstructor(JoinPoint joinPoint){
+        LogUtils.i(TAG, "aroundCallConstructor() called");
+    }
+
+}
