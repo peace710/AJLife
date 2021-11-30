3645bce96972a3872a8b45c9dd76d565690d3fd8
peace710 <wxxinyun@163.com>
2021-11-30 10:45:54 +0800

    add two sum code using c

---
+++ LeetCode/1.两数之和/Solution.c
@@ -0,0 +1,18 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int* result = NULL;
+    for (int i = 0 ; i < numsSize - 1;i++){
+        for (int j = i + 1 ; j < numsSize;j++){
+            if (nums[i] + nums[j] == target){
+                result = (int*)malloc(sizeof(int)*2);
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    return result;
+}
